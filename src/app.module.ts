3ec8137ede6d497df000c0a7f8d7ac0c50a7e351
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-09-03 18:30:51 -0300

    feat: created prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { PrismaService } from './prisma/prisma.service'
 
 @Module({
   // Importar mais módulos
@@ -9,6 +10,6 @@ import { AppService } from './app.service'
   controllers: [AppController],
   // Providers é todo o "resto", tudo que vai precisar injetar em outras classes
   // exemplo, uma classe que é um repositório do banco de dados, classe que é um caso de uso, classe que faz envio de email, e por aí vai
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
