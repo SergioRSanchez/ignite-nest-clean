6b42a8e0f3574ef35ea370002dda0156c2e24e5b
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-09-04 11:59:08 -0300

    feat: implement user creation controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { PrismaService } from './prisma/prisma.service'
+import { CreateAccountController } from './controllers/create-account.controller'
 
 @Module({
   // Importar mais módulos
   imports: [],
   // Controllers é tudo que recebe requisição http
-  controllers: [AppController],
+  controllers: [CreateAccountController],
   // Providers é todo o "resto", tudo que vai precisar injetar em outras classes
   // exemplo, uma classe que é um repositório do banco de dados, classe que é um caso de uso, classe que faz envio de email, e por aí vai
-  providers: [AppService, PrismaService],
+  providers: [PrismaService],
 })
 export class AppModule {}
