189d79829549d3f5f5f6a8731af0dad1812034ea
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-09-05 15:41:27 -0300

    feat: implemented jwt token generation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
 
 @Module({
   // Importar mais módulos
@@ -15,7 +16,7 @@ import { AuthModule } from './auth/auth.module'
     AuthModule,
   ],
   // Controllers é tudo que recebe requisição http
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, AuthenticateController],
   // Providers é todo o "resto", tudo que vai precisar injetar em outras classes
   // exemplo, uma classe que é um repositório do banco de dados, classe que é um caso de uso, classe que faz envio de email, e por aí vai
   providers: [PrismaService],
