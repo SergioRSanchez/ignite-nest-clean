3355ad02408c1303b30e0886c8f6a99e312717d4
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-09-11 18:14:48 -0300

    feat: install vitest and add path aliases

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
 import { PassportModule } from '@nestjs/passport'
 
-import { Env } from 'src/env'
+import { Env } from '@/env'
 import { JwtStrategy } from './jwt.strategy'
 
 @Module({
@@ -26,4 +26,4 @@ import { JwtStrategy } from './jwt.strategy'
   ],
   providers: [JwtStrategy],
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -9,8 +9,8 @@ import {
 } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 
-import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
-import { PrismaService } from 'src/prisma/prisma.service'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { PrismaService } from '@/prisma/prisma.service'
 import { compare } from 'bcryptjs'
 
 const authenticateBodySchema = z.object({
