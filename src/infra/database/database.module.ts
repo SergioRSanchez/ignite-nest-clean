e394fe7627633a218e4f8ef02916debd5c2df6e9
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-09-19 20:39:17 -0300

    feat: implemented prisma repositories

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common'
+
+import { PrismaService } from './prisma/prisma.service'
+import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions.repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
+import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
+import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
+
+@Module({
+  // Providers é todo o "resto", tudo que vai precisar injetar em outras classes
+  // exemplo, uma classe que é um repositório do banco de dados, classe que é um caso de uso, classe que faz envio de email, e por aí vai
+  providers: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+  ],
+  exports: [
+    PrismaService,
+    PrismaQuestionsRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswersRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+  ],
+})
+export class DatabaseModule {}
--- src/infra/http/controllers/fetch-recent-questions.controller.e2e-spec.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.e2e-spec.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 
 import { INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
-import { PrismaService } from '@/infra/prisma/prisma.service'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 import { AppModule } from '@/infra/app.module'
 import { JwtService } from '@nestjs/jwt'
