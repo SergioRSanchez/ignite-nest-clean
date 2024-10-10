0324aaad7e394cd083dacba5a97c68e679ccfa95
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-10-10 16:49:22 -0300

    feat: implemented createMany and deleteMany methods

--- test/repositories/in-memory-question-attachments-repository.ts
+++ test/repositories/in-memory-question-attachments-repository.ts
@@ -2,9 +2,21 @@ import { QuestionAttachmentsRepository } from '@/domain/forum/application/reposi
 import { QuestionAttachment } from '@/domain/forum/enterprise/entities/question-attachment'
 
 export class InMemoryQuestionAttachmentsRepository
-  // eslint-disable-next-line prettier/prettier
-  implements QuestionAttachmentsRepository {
+  implements QuestionAttachmentsRepository
+{
   public items: QuestionAttachment[] = []
+
+  async createMany(attachments: QuestionAttachment[]): Promise<void> {
+    this.items.push(...attachments)
+  }
+
+  async deleteMany(attachments: QuestionAttachment[]): Promise<void> {
+    const questionAttachments = this.items.filter((item) => {
+      return !attachments.some((attachment) => attachment.equals(item))
+    })
+
+    this.items = questionAttachments
+  }
 
   async findManyByQuestionId(questionId: string) {
     const questionAttachments = this.items.filter(
