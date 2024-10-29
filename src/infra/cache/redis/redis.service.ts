bd3899d80d26d7a2be265bd6d05259facd26203e
Sergio Sanchez <ssanchezfilho@gmail.com>
2024-10-29 13:08:19 -0300

    feat: install ioredis and implemented redis service

---
+++ src/infra/cache/redis/redis.service.ts
@@ -0,0 +1,18 @@
+import { OnModuleDestroy } from '@nestjs/common'
+
+import { Redis } from 'ioredis'
+
+export class RedisService extends Redis implements OnModuleDestroy {
+  constructor() {
+    super({
+      // host: 'localhost',
+      host: '127.0.0.1',
+      port: 6379,
+      db: 0,
+    })
+  }
+
+  onModuleDestroy() {
+    return this.disconnect()
+  }
+}
